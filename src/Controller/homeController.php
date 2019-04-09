36f22f0da48eff7be8dfb815384d9dcb3b58b1ec
MarionLeHerisson <marion.hurteau1@gmail.com>
2019-04-09 15:31:55 +0200

    Our first landing page !

---
+++ src/Controller/homeController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marion
+ * Date: 09/04/2019
+ * Time: 12:38
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class homeController extends AbstractController
+{
+    /**
+     * @Route(path="/", name="homepage")
+     */
+    public function homepageAction()
+    {
+        return $this->render('homepage.html.twig', [
+
+        ]);
+    }
+}
