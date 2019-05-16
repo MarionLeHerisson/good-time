2fa962212e831a8ff01b3d8ecdb58a66628c59af
MarionLeHerisson <marion.hurteau1@gmail.com>
2019-05-16 15:02:35 +0200

    better with a password

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -4,16 +4,27 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends BaseFixture
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function loadData(ObjectManager $manager)
     {
         $this->createMany(10, 'main_users', function($i) {
             $user = new User();
             $user->setEmail(sprintf($this->faker->email));
             $user->setName(sprintf($this->faker->firstName . ' ' . $this->faker->lastName));
-            $user->setPassword('123456');
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                '123456'
+            ));
 
             return $user;
         });
