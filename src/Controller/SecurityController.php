cabd993a2a8a39bce62918e05fa053712f21e84c
MarionLeHerisson <marion.hurteau1@gmail.com>
2019-05-29 12:39:42 +0200

    fixed subsciption :grimace:

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -27,6 +27,7 @@ class SecurityController extends AbstractController
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
+        dump($error);
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
@@ -61,7 +62,7 @@ class SecurityController extends AbstractController
 
             $user->setPassword($passwordEncoder->encodePassword(
                 $user,
-                $user->getPassword()
+                $form_user['plainPassword']->getData()
             ));
 
             if (true === $form_user['isBarOwner']->getData()) {
