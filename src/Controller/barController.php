ce6c62f74b61a4836ea7bf6b0a7b93a4eac5bed7
MarionLeHerisson <marion.hurteau1@gmail.com>
2019-06-04 15:15:29 +0200

    gestion des reservations wip

--- src/Controller/barController.php
+++ src/Controller/barController.php
@@ -38,10 +38,83 @@ class barController extends AbstractController
                 'le faire <a href="/inscription-bar">ici</a>.');
         }
 
+        $reservations = [
+            'waiting' => [
+                [
+                    'name' => 'John Doe',
+                    'phone' => '0123456789',
+                    'email' => 'john.doe@mail.fr',
+                    'nb_persons' => 20,
+                    'date' => '1565632800'
+                ],
+                [
+                    'name' => 'Karen Doe',
+                    'phone' => '0123456789',
+                    'email' => 'karen.doe@mail.fr',
+                    'nb_persons' => 12,
+                    'date' => '1565632800'
+                ],
+                [
+                    'name' => 'Lauren Doe',
+                    'phone' => '0123456789',
+                    'email' => 'lauren.doe@mail.fr',
+                    'nb_persons' => 3,
+                    'date' => '1565632800'
+                ],
+            ],
+            'accepted' => [
+                [
+                    'name' => 'Morty Doe',
+                    'phone' => '0123456789',
+                    'email' => 'morty.doe@mail.fr',
+                    'nb_persons' => 9,
+                    'date' => '1565632800'
+                ],
+                [
+                    'name' => 'Nathalia Doe',
+                    'phone' => '0123456789',
+                    'email' => 'nat.doe@mail.fr',
+                    'nb_persons' => 8,
+                    'date' => '1565632800'
+                ],
+                [
+                    'name' => 'Oliver Doe',
+                    'phone' => '0123456789',
+                    'email' => 'oliver.doe@mail.fr',
+                    'nb_persons' => 28,
+                    'date' => '1565632800'
+                ],
+            ],
+            'denied' => [
+                [
+                    'name' => 'Patrick Doe',
+                    'phone' => '0123456789',
+                    'email' => 'pat.doe@mail.fr',
+                    'nb_persons' => 200,
+                    'date' => '1565632800'
+                ],
+                [
+                    'name' => 'Qi Doe',
+                    'phone' => '0123456789',
+                    'email' => 'qi.doe@mail.fr',
+                    'nb_persons' => 45,
+                    'date' => '1565632800'
+                ],
+                [
+                    'name' => 'Rick Doe',
+                    'phone' => '0123456789',
+                    'email' => 'rick.doe@mail.fr',
+                    'nb_persons' => 7,
+                    'date' => '1565632800'
+                ],
+            ]
+        ];
+
         return $this->render('bar.html.twig', [
             'bar'                  => $bar,
-            'hasNewReservations'   => "true",  // TODO
-            'isReservationsActive' => " active",
+            'hasNewReservations'   => "true", // TODO
+            'isReservationsActive' => " active", // TODO
+            'reservations'         => $reservations
         ]);
     }
 }
