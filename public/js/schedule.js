ce8a8c993ec9c94b93c36fbad8a5f570cdeb8107
MarionLeHerisson <marion.hurteau1@gmail.com>
2019-06-08 16:34:04 +0200

    tests with jquery schedule calendar - not working :shrugging:

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -1,33 +1,54 @@
+var element;
 let changeBackground = function (e) {
-        console.log(e);
-        let element = $(this);
-        let newClass = ($('#happySwitch').is(':checked')) ? 'happy' : 'open';
+        e.preventDefault();
+        element = $(this);
 
-        if(element.hasClass('schedule-closed')) {
-            element.removeClass('schedule-closed');
-            element.addClass('schedule-'+newClass);
-        } else {
-            if(element.hasClass('schedule-happy') && newClass === 'happy') {
-                element.removeClass('schedule-happy');
-                element.addClass('schedule-open');
-            } else if (element.hasClass('schedule-open') && newClass === 'happy') {
-                element.removeClass('schedule-open');
-                element.addClass('schedule-happy');
+        document.body.onmousedown = function(element) {
+            let newClass = ($('#happySwitch').is(':checked')) ? 'happy' : 'open';
+
+            if(element.hasClass('schedule-closed')) {
+                element.removeClass('schedule-closed');
+                element.addClass('schedule-'+newClass);
             } else {
-                element.removeClass('schedule-open');
-                element.removeClass('schedule-happy');
-                element.addClass('schedule-closed');
+                if(element.hasClass('schedule-happy') && newClass === 'happy') {
+                    element.removeClass('schedule-happy');
+                    element.addClass('schedule-open');
+                } else if (element.hasClass('schedule-open') && newClass === 'happy') {
+                    element.removeClass('schedule-open');
+                    element.addClass('schedule-happy');
+                } else {
+                    element.removeClass('schedule-open');
+                    element.removeClass('schedule-happy');
+                    element.addClass('schedule-closed');
+                }
             }
         }
-
     },
 
     binds = function() {
-        $('.js-schedule-cell').click(changeBackground);
+        // $('.js-schedule-cell').on('click', changeBackground);
+        $('.js-schedule-cell').on('mouseover', changeBackground);
     };
 
 $(document).ready(function () {
     binds();
 });
 
-
+// $(function () {
+    //
+    // $('#schedule').jqs();
+    //
+    // $("#schedule").jqs({
+    //     days: ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"],
+    //     hour: 24,
+    //     data: [
+    //         {
+    //             day: 0,
+    //             periods: [
+    //                 ["10pm", "12am"],
+    //                 ["12am", "2:30am"]
+    //             ]
+    //         }
+    //     ]
+    // });
+// });
